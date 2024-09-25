8bfd95473a2282c1197218041b46a60b25d3efb7
Samuel <samucarcoverde@gmail.com>
2024-09-25 18:22:23 -0300

    community realizado

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,4 +1,24 @@
+import Link from 'next/link'
+import classes from './page.module.css'
+
 export default function MealsPage(){
-    return <h1>Meals Page</h1>;
+   return <>
+    <header className={classes.header}>
+        <h1>
+         Comidas GOSTOSAS, criadas <span className={classes.highlight}>para você</span>   
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself. É fácil e rápido!</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">
+            Share Your Favorite Recipe
+          </Link>
+
+        </p>
+    </header>
+    <main className={classes.main}>
+        
+    </main>
+   
+   </>
 
 }
